bed3b35a02016336d4b6c56265769c5b29055f6a
Bob Callaway <bcallawa@redhat.com>
2016-08-15 07:16:24 -0400

    day2

--- server.js
+++ server.js
@@ -1,33 +1,64 @@
 'use strict';
 
-const express = require('express');
-var bodyParser = require('body-parser');
-var phantomjs = require('phantomjs-prebuilt')
-let exec = require('child_process').exec;
+var path = require('path');
+var childProcess = require('child_process');
+var phantomJsPath = require('phantomjs-prebuilt').path;
+var mktemp = require('mktemp');
 
-// Express Stuff
-const PORT = 8080;
-const app = express();
-app.use(bodyParser.json());
+exports.handler = function (event, context, callback) { 
+  if (event.data) { 
+    generateImage(event.data, function(err, result) {
+      if (err) {
+        return callback(null, {error: result});
+      }
+      callback(null, {image: result});
+    });
+  }
+  else {
+    callback(null, {error: 'bad query'});
+  }
+};
 
-app.get('/', function (req, res) {
-  res.json(req.body);
+exports.generateImage = function(data, callback) {
 
-/*
-- generate SVG from input JSON
-- call phantomjs to create png from SVG
-- return png in response
-*/
+  var imageOutputFile = mktemp.createFile("XXXXX.html", function (err, path){
+    if (err) 
+      return callback(true, err);
+    else {
+      /*
+      - TODO: generate SVG from input JSON
+      */
+      // data
+    }
+  };
 
-/*
-var program = phantomjs.exec('phantomjs-script.js', 'arg1', 'arg2')
-program.stdout.pipe(process.stdout)
-program.stderr.pipe(process.stderr)
-program.on('exit', code => {
-  // do something on end 
-})
-*/
-});
+  var childArgs = [path.join(__dirname, 'phantomjs-script.js')];
+  var phantom = childProcess.execFile(phantomJsPath, childArgs, { 
+    env: {
+      URL: 'cb.html' //TODO: change me
+    },
+    maxBuffer: 2048*1024
+  });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+  var stdout = '';
+  var stderr = '';
+
+  phantom.stdout.on('data', function(data) {
+    stdout += data;
+  });
+
+  phantom.stderr.on('data', function(data) {
+    stderr += data;
+  });
+
+  phantom.on('uncaughtException', function(err) {
+    console.log('uncaught exception: ' + err);
+  });
+
+  phantom.on('exit', function(exitCode) {
+    if (exitCode !== 0) {
+      return callback(true, stderr);
+    }
+    callback(null, new Buffer(stdout).toString('base64'));
+  });
+};
