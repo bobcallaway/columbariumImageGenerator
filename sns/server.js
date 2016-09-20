8579b493868ffef57edea1ad6fe31e8f20d5a68a
Bob Callaway <bcallawa@redhat.com>
2016-09-20 06:35:31 -0400

    add sns

---
+++ sns/server.js
@@ -0,0 +1,40 @@
+var AWS = require('aws-sdk');
+var _ = require('underscore');
+
+exports.handler = function (event, context, callback) { 
+  if (event) { 
+    sendSMS(event, function(err, result) {
+      if (err) {
+        return callback(null, {error: result});
+      }
+      callback(null, result);
+    });
+  }
+  else {
+    callback(null, {error: 'bad query'});
+  }
+};
+
+function sendSMS(event, callback) {
+  var sns = new AWS.SNS();
+
+  var errors = [];
+
+  _.each(event.phones, function(phone){
+    var params = {
+      PhoneNumber: phone,
+      Message: event.message, 
+      Subject: (urgent?"URGENT ":"") + "StMM Columbarium:",
+      AWS.SNS.SMS.SenderID: "StMM",
+      AWS.SNS.SMS.SMSType: "Transactional"
+    };
+    sns.publish(params, function(err, data) {
+      if (err) errors.push(err);
+    }); 
+  });
+  if (err.length) > 0){
+    callback(true, errors);
+  }
+  else
+    callback(null, {success: true});
+};
